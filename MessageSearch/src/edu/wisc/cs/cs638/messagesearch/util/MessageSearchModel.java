be5a52d80a9876c510ec36ae878a4f08900b0ee6
Theodora Hinkle <thinkle@wisc.edu>
2012-03-13 17:28:32 -0500

    adding MessageSearchModel.java class - draft

---
+++ MessageSearch/src/edu/wisc/cs/cs638/messagesearch/util/MessageSearchModel.java
@@ -0,0 +1,64 @@
+package edu.wisc.cs.cs638.messagesearch;
+
+import java.util.List;
+
+public class MessageSearchModel {
+	
+	List<MessageSource> _sources;
+	
+	public void search(Search filter) {
+		
+	}
+	
+	public Search getCurrentSearch() {
+		
+		return null;
+	}
+	
+	public List<Message> getStarredMessages() {
+		
+		return null;
+	}
+	
+	public List<Message> getRecentSearches() {
+		
+		return null;
+	}
+	
+	public boolean addStarredMessage(Message msg) {
+		return true;
+	}
+	
+	public boolean removeStarredMessage(Message msg) {
+		return true;
+	}
+	
+	/*
+	 * if a single source is selected, returns list of contacts from that source
+	 * otherwise returns null
+	 */
+	public List<Contact> getContacts() {
+		return null;
+	}
+	
+	/*
+	 * Returns the currently selected message sources
+	 */
+	public List<MessageSource> getSearchSources() {
+		return _sources;
+	}
+	
+	public void addSearchSource(MessageSource source) {
+		_sources.add(source);
+	}
+	
+	public boolean removeSearchSource(MessageSource source) {
+		return _sources.remove(source);
+	}
+	
+	
+	public List<Message> getThread(Message msg) {
+		return null;
+	}
+	
+}
