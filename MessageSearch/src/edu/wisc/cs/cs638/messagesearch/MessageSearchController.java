dc589ec8018d29ab37d388dac41520030f627238
Benjamin Frisch <git@benfrisch.net>
2012-03-12 20:23:57 -0500

    Adding a base MessageSearchController.

---
+++ MessageSearch/src/edu/wisc/cs/cs638/messagesearch/MessageSearchController.java
@@ -0,0 +1,44 @@
+package edu.wisc.cs.cs638.messagesearch;
+
+import android.view.View;
+
+public class MessageSearchController {
+	private final MessageSearchController instance = new MessageSearchController();
+	
+	public MessageSearchController getInstance() {
+		return instance;
+	}
+	
+	public class SearchButtonListener implements View.OnClickListener {
+		public void onClick(View v) {
+			
+		}
+	}
+	
+	public class StarredMessageListener implements View.OnClickListener {
+		public void onClick(View v) {
+		}
+	}
+	
+	public class RecentSearchListener implements View.OnClickListener {
+		public void onClick(View v) {
+		}
+	}
+	
+	public class ContactSourceListener implements View.OnClickListener {
+		public void onClick(View v) {
+		}
+	}
+	
+	public class ContactSelector implements View.OnClickListener {
+		public void onClick(View v) {
+			
+		}
+	}
+	
+	public class ResultSelected implements View.OnClickListener {
+		public void onClick(View v) {
+			
+		}
+	}
+}
