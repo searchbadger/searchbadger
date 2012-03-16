5959039bb8c8084497d5bef3c4e43da253230812
Kong Yang <kongyang80@gmail.com>
2012-03-16 18:18:43 -0500

    Test project did not seem to be working. Fixed it so test passes.
    Signed-off-by: Kong Yang <kongyang80@gmail.com>

--- MessageSearchTest/src/edu/wisc/cs/cs638/messagesearch/test/SearchActivityTest.java
+++ MessageSearchTest/src/edu/wisc/cs/cs638/messagesearch/test/SearchActivityTest.java
@@ -1,34 +1,41 @@
 package edu.wisc.cs.cs638.messagesearch.test;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
-import edu.wisc.cs.cs638.messagesearch.R;
 import edu.wisc.cs.cs638.messagesearch.view.SearchActivity;
 
-public class SearchActivityTest extends ActivityInstrumentationTestCase2<SearchActivity>{
+public class SearchActivityTest extends
+		ActivityInstrumentationTestCase2<SearchActivity> {
 
 	private SearchActivity testActivity;
 	private LinearLayout dateLayout, contactsLayout, sentLayout;
 	private CheckBox dateCheck, contactsCheck, sentCheck;
-	
+
 	public SearchActivityTest() {
-		super("edu.wisc.cs.cs638.messagesearch.view", SearchActivity.class);
+		super("edu.wisc.cs.cs638.messagesearch", SearchActivity.class);
 	}
 
-	protected void setUp() throws Exception{
+	protected void setUp() throws Exception {
 		super.setUp();
 		testActivity = this.getActivity();
-		dateLayout = (LinearLayout) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterDateOptions);
-        contactsLayout = (LinearLayout) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterContactsOptions);
-        sentLayout = (LinearLayout) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterSentReceivedOptions);
-        dateCheck = (CheckBox) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterDate);
-        contactsCheck = (CheckBox) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterContacts);
-        sentCheck = (CheckBox) testActivity.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterSentReceived);
+		dateLayout = (LinearLayout) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterDateOptions);
+		contactsLayout = (LinearLayout) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterContactsOptions);
+		sentLayout = (LinearLayout) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.linearFilterSentReceivedOptions);
+		dateCheck = (CheckBox) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterDate);
+		contactsCheck = (CheckBox) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterContacts);
+		sentCheck = (CheckBox) testActivity
+				.findViewById(edu.wisc.cs.cs638.messagesearch.R.id.checkBoxFilterSentReceived);
 	}
-	
-	protected void testPreconditions() {
+
+	public void testPreconditions() {
 		assertNotNull(testActivity);
 		assertNotNull(dateLayout);
 		assertNotNull(contactsLayout);
@@ -37,26 +44,25 @@ public class SearchActivityTest extends ActivityInstrumentationTestCase2<SearchA
 		assertNotNull(contactsCheck);
 		assertNotNull(sentCheck);
 	}
-	
-	protected void testToggleDate() {
-		testActivity.toggleFilterSentReceived();
+
+	@UiThreadTest
+	public void testToggleDate() {
+		sentCheck.performClick();
+		// testActivity.toggleFilterSentReceived();
 		assertTrue(sentCheck.isChecked());
-		assertEquals("Sent/received layout visibility",sentLayout.getVisibility(), View.VISIBLE);
-		testActivity.toggleFilterSentReceived();
+		assertEquals("Sent/received layout visibility",
+				sentLayout.getVisibility(), View.VISIBLE);
+		// testActivity.toggleFilterSentReceived();
+		sentCheck.performClick();
 		assertTrue(!sentCheck.isChecked());
-		assertEquals("Sent/received layout visibility",sentLayout.getVisibility(), View.GONE);
+		assertEquals("Sent/received layout visibility",
+				sentLayout.getVisibility(), View.GONE);
 	}
-	
-	protected void testToggleContacts() {
-		
+
+	public void testToggleContacts() {
 	}
-	
-	protected void testToggleSentReceived() {
-		
-	}
-	
-	protected void tearDown() {
-		
+
+	public void testToggleSentReceived() {
 	}
 
 }
