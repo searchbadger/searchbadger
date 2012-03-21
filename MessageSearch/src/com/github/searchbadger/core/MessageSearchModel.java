a6246a3a938b5ed4e89aa58d38c260ddaf0d7e9a
Kong Yang <kongyang80@gmail.com>
2012-03-20 23:53:47 -0500

    Fixed bug due to package name change. Partially fixed the search function so it actually returns some results.
    Signed-off-by: Kong Yang <kongyang80@gmail.com>

--- MessageSearch/src/com/github/searchbadger/core/MessageSearchModel.java
+++ MessageSearch/src/com/github/searchbadger/core/MessageSearchModel.java
@@ -28,7 +28,7 @@ public class MessageSearchModel {
 	//private Cursor searchResultCursor;
 	private List<Map<String,String>> searchResults;
 	private List<Message> searchResultMessages;
-	private final static String projectionList[] = {"_id", "person", "date", "body"};
+	private final static String projectionList[] = {"_id", "thread_id", "address", "person", "date", "body", "type"};
 	
 	
 	public static MessageSearchModel getInstance() {
@@ -113,50 +113,54 @@ public class MessageSearchModel {
 		
 		// Make query to content provider and store cursor to table returned
 		//searchResultCursor = Activity.managedQuery(uri, projectionList, selection, selectionArgArray, "");
-		Cursor searchResultCursor = MessageSearchApplication.getAppContext().getContentResolver().query(uri, projectionList, selection, selectionArgList, "date DESC");
+		Cursor searchResultCursor = MessageSearchApplication.getAppContext().getContentResolver().query(uri, projectionList, selection, null, "date DESC");
 		
 		searchResults = new ArrayList<Map<String,String>>();
 		searchResultMessages = new ArrayList<Message>();
-		
+
 		if (searchResultCursor != null) {
-            try {
-                    int count = searchResultCursor.getCount();
-                    if (count > 0) {
-                    	searchResultCursor.moveToFirst();
+			try {
+				int count = searchResultCursor.getCount();
+				if (count > 0) {
+					searchResultCursor.moveToFirst();
+					do {
 
-//                          String[] columns = cursor.getColumnNames();
-//                          for (int i=0; i<columns.length; i++) {
-//                                  Log.v("columns " + i + ": " + columns[i] + ": "
-//                                                  + cursor.getString(i));
-//                          }
+						// String[] columns = cursor.getColumnNames();
+						// for (int i=0; i<columns.length; i++) {
+						// Log.v("columns " + i + ": " + columns[i] + ": "
+						// + cursor.getString(i));
+						// }
 
-                            long messageId = searchResultCursor.getLong(0);
-                            long threadId = searchResultCursor.getLong(1);
-                            String address = searchResultCursor.getString(2);
-                            long contactId = searchResultCursor.getLong(3);
-                            String contactId_string = String.valueOf(contactId);
-                            long timestamp = searchResultCursor.getLong(4);
-                            String body = searchResultCursor.getString(5);
-                            
-                            Contact c = new Contact(contactId, MessageSource.SMS, contactId_string, null);
+						long messageId = searchResultCursor.getLong(0);
+						long threadId = searchResultCursor.getLong(1);
+						String address = searchResultCursor.getString(2);
+						long contactId = searchResultCursor.getLong(3);
+						String contactId_string = String.valueOf(contactId);
+						long timestamp = searchResultCursor.getLong(4);
+						String body = searchResultCursor.getString(5);
 
-                            Message msg = new Message(c, messageId, threadId, body, false);
-                            searchResultMessages.add(msg);
-                            
-                            HashMap<String, String> map = new HashMap<String,String>();
-                            map.put("Message", body);
-                            map.put("Time", ((Long)timestamp).toString());
-                            map.put("FromAddress", address);
-                            map.put("ID", ((Long)messageId).toString());
-                            map.put("ThreadID", ((Long)threadId).toString());
-                            map.put("ContactID", contactId_string);
-                            searchResults.add(map);
+						Contact c = new Contact(contactId, MessageSource.SMS,
+								contactId_string, null);
 
-                    }
-            } finally {
-                    searchResultCursor.close();
-            }
-    }          
+						Message msg = new Message(c, messageId, threadId, body,
+								false);
+						searchResultMessages.add(msg);
+
+						HashMap<String, String> map = new HashMap<String, String>();
+						map.put("Message", body);
+						map.put("Time", ((Long) timestamp).toString());
+						map.put("FromAddress", address);
+						map.put("ID", ((Long) messageId).toString());
+						map.put("ThreadID", ((Long) threadId).toString());
+						map.put("ContactID", contactId_string);
+						searchResults.add(map);
+					} while (searchResultCursor.moveToNext() == true);
+
+				}
+			} finally {
+				searchResultCursor.close();
+			}
+		}
 
 	}
 	
