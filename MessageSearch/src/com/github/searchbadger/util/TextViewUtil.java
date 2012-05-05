92f42de9a96e99afba7ce8b8422e92813506fa1b
Kong Yang <kongyang80@gmail.com>
2012-05-05 00:00:37 -0500

    Highlight found text in search result
    Signed-off-by: Kong Yang <kongyang80@gmail.com>

--- MessageSearch/src/com/github/searchbadger/util/TextViewUtil.java
+++ MessageSearch/src/com/github/searchbadger/util/TextViewUtil.java
@@ -1,18 +1,23 @@
 package com.github.searchbadger.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.text.Editable;
 import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
+import android.util.Log;
 
 import com.github.searchbadger.R;
 import com.github.searchbadger.core.SearchBadgerApplication;
 
 public class TextViewUtil {
 
-	// change author color here
+	// change color here
 	static public String authorTextColor = SearchBadgerApplication.getAppContext().getResources().getString(R.color.light_blue);
 	static public String bodySelectedTextColor = SearchBadgerApplication.getAppContext().getResources().getString(R.color.light_green);
+	static public String bodyRegexColor = SearchBadgerApplication.getAppContext().getResources().getString(R.color.yellow);
 
 	static public Editable formatMessage(String author, String message) {
 		SpannableStringBuilder editable = new SpannableStringBuilder();
@@ -27,6 +32,16 @@ public class TextViewUtil {
 		Spanned spanned = Html.fromHtml("<font color='#" + authorTextColor.substring(3) + "'>" + author + ": </font>");
 		editable.append(spanned);
 		spanned = Html.fromHtml("<font color='#" + bodySelectedTextColor.substring(3) + "'>" + message + "</font>");
+		editable.append(spanned);
+		return editable;
+	}
+	
+	static public Editable formatMessageSearch(String author, String message, Pattern pattern) {
+		SpannableStringBuilder editable = new SpannableStringBuilder();
+		Spanned spanned = Html.fromHtml("<font color='#" + authorTextColor.substring(3) + "'>" + author + ": </font>");
+		editable.append(spanned);
+		Matcher matcher = pattern.matcher(message);
+		spanned = Html.fromHtml(matcher.replaceAll("<font color='#" + bodyRegexColor.substring(3) + "'>$0</font>"));
 		editable.append(spanned);
 		return editable;
 	}
